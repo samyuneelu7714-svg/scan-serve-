a32f3ecf2e5b698bf74a96475c00c7516972cd39
samyuneelu7714-svg <samyuneelu7714@gmail.com>
2026-08-21 12:08:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
-function generateBarcode() {
+function generateCode() {
 
     let food = document.getElementById("foodName").value.trim().toLowerCase();
 
-    if (food !== "mango" && food !== "apple" && food !== "banana") {
-        alert("Please enter Mango, Apple or Banana");
+    let page = "";
+
+    if (food === "mango") {
+        page = "mango.html";
+    }
+    else if (food === "apple") {
+        page = "apple.html";
+    }
+    else if (food === "banana") {
+        page = "banana.html";
+    }
+    else if (food === "oreo") {
+        page = "oreo.html";
+    }
+    else {
+        alert("Please enter Mango, Apple, Banana or Oreo.");
         return;
     }
 
+    // Change this to your website URL
+    let url = "https://scanandserve.onrender.com/" + page;
+
+    // Generate Barcode
     JsBarcode("#barcode", food, {
         format: "CODE128",
         width: 2,
-        height: 100,
+        height: 80,
         displayValue: true
     });
 
-}
-
-function generateQR() {
-
-    let food = document.getElementById("foodName").value.trim().toLowerCase();
-
-    if (food !== "mango" && food !== "apple" && food !== "banana") {
-        alert("Please enter Mango, Apple or Banana");
-        return;
-    }
-
+    // Clear old QR code
     document.getElementById("qrcode").innerHTML = "";
 
-    const url = window.location.origin + "/recipe.html?fruit=" + food;
-
+    // Generate new QR code
     new QRCode(document.getElementById("qrcode"), {
         text: url,
         width: 220,
         height: 220
     });
-
 }
